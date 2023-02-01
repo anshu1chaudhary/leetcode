a242dc8afada5470206bc57018a1f3601ea7ad7e
anshu1chaudhary <107908541+anshu1chaudhary@users.noreply.github.com>
2023-02-01 14:51:23 +0530

    Time: 11 ms (62.24%), Space: 54.7 MB (74.30%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       
+        HashSet<Integer> hset = new HashSet<Integer>();
+       
+        for (int idx = 0; idx < nums.length; idx ++){
+            
+            if (hset.contains(nums[idx])){
+                return true;
+            }
+        
+            hset.add(nums[idx]);
+        }
+    
+        return false;
+    }
+}
